0d4790d5e605c54ec1bb6accd9485e213f43a1ea
javier200151 <javier200151@gmail.com>
2021-02-25 08:27:36 -0500

    heroku

--- covidAnalyzerApi/src/main/java/eci/arsw/covidanalyzer/CovidAggregateController.java
+++ covidAnalyzerApi/src/main/java/eci/arsw/covidanalyzer/CovidAggregateController.java
@@ -26,16 +26,71 @@ public class CovidAggregateController {
             return new ResponseEntity<>("Error",HttpStatus.NOT_FOUND);
         }          
     }
-
+    @RequestMapping(value = "/covid/result/true-negative", method = RequestMethod.POST)
+    public ResponseEntity addTrueNegativeResult(Result result) {
+        boolean post = covidAggregateService.aggregateResult(result, ResultType.TRUE_NEGATIVE);
+        if(post){
+            return new ResponseEntity<>("Hecho",HttpStatus.ACCEPTED);
+        }else{
+            return new ResponseEntity<>("Error",HttpStatus.NOT_FOUND);
+        } 
+    }
+    
+    @RequestMapping(value = "/covid/result/false-positive", method = RequestMethod.POST)
+    public ResponseEntity addFalsePositiveResult(Result result) {
+        boolean post = covidAggregateService.aggregateResult(result, ResultType.FALSE_POSITIVE);
+        if(post){
+            return new ResponseEntity<>("Hecho",HttpStatus.ACCEPTED);
+        }else{
+            return new ResponseEntity<>("Error",HttpStatus.NOT_FOUND);
+        } 
+    }
+    @RequestMapping(value = "/covid/result/false-negative", method = RequestMethod.POST)
+    public ResponseEntity addFalseNegativeResult(Result result) {
+        //TODO
+        boolean post = covidAggregateService.aggregateResult(result, ResultType.FALSE_NEGATIVE);
+        if(post){
+            return new ResponseEntity<>("Hecho",HttpStatus.ACCEPTED);
+        }else{
+            return new ResponseEntity<>("Error",HttpStatus.NOT_FOUND);
+        } 
+    }
+    
+    
+    
+    
+    
+    
+    
     //TODO: Implemente todos los metodos GET que hacen falta.
 
     @RequestMapping(value = "/covid/result/true-positive", method = RequestMethod.GET)
     public ResponseEntity getTruePositiveResult() {
         //TODO
-        //covidAggregateService.getResult(ResultType.TRUE_POSITIVE);
-        return ResponseEntity.ok("Hello World");
+        //covidAggregateService.getResult(ResultType.TRUE_POSITIVE);      
+        return ResponseEntity.ok("Hello World TruePositive");
     }
-
+    
+    @RequestMapping(value = "/covid/result/true-negative", method = RequestMethod.GET)
+    public ResponseEntity getTrueNegativeResult() {
+        //TODO
+        //covidAggregateService.getResult(ResultType.TRUE_POSITIVE);      
+        return ResponseEntity.ok("Hello World TrueNegative");
+    }
+    
+    @RequestMapping(value = "/covid/result/false-positive", method = RequestMethod.GET)
+    public ResponseEntity getFalsePositiveResult() {
+        //TODO
+        //covidAggregateService.getResult(ResultType.TRUE_POSITIVE);      
+        return ResponseEntity.ok("Hello World FalsePositive");
+    }
+    
+    @RequestMapping(value = "/covid/result/false-negative", method = RequestMethod.GET)
+    public ResponseEntity getFalseNegativeResult() {
+        //TODO
+        //covidAggregateService.getResult(ResultType.TRUE_POSITIVE);      
+        return ResponseEntity.ok("Hello World FalseNegative");
+    }
 
     //TODO: Implemente el método.
 
